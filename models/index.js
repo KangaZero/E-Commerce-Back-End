67c43d4e1f6ae684027601c44b49bb4893bb73c8
Samuel <samuelyongw@gmail.com>
2022-10-31 21:00:07 +1100

    lint-fix applied

--- models/index.js
+++ models/index.js
@@ -1,30 +1,30 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE'
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
 });
 // Products belongsTo Category
 
 Category.hasMany(Product, {
-  foreignKey:  'category_id'
+  foreignKey:  "category_id"
 });
 // Categories have many Products
 
 Product.belongsToMany(Tag, {
-  as: 'tag_product_id',
-  foreignKey: 'product_id',
+  as: "tag_product_id",
+  foreignKey: "product_id",
   through: ProductTag,
 });
 // Products belongToMany Tags (through ProductTag)
 
-Tag.belongsToMany(Product, { 
-  as: 'product_tag_id', 
-  foreignKey: 'tag_id',
+Tag.belongsToMany(Product, {
+  as: "product_tag_id",
+  foreignKey: "tag_id",
   through: ProductTag,
 });
 // Tags belongToMany Products (through ProductTag)
